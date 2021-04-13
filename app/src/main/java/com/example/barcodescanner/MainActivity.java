928a6c5e596573f4d6f16595d4ffa208bfa2f12b
Bholanath Barik <bholanathbarik9748@gmail.com>
2021-04-14 01:17:11 +0530

    crete privacy,terms and condition and company rule and connect to each other and also home page i mean ActivityMain.java and improve docs

--- app/src/main/java/com/example/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -8,13 +8,15 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView imageView;
+    private ImageView imageView, imageView1, imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView6);
+        imageView1 = findViewById(R.id.imageView9);
+        imageView2 = findViewById(R.id.imageView12);
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,10 +24,34 @@ public class MainActivity extends AppCompatActivity {
                 prv();
             }
         });
+
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prv1();
+            }
+        });
+
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prv2();
+            }
+        });
     }
 
     private void prv() {
         Intent intent = new Intent(this, privacy.class);
+        startActivity(intent);
+    }
+
+    private void prv1() {
+        Intent intent = new Intent(MainActivity.this, Termandcondition.class);
+        startActivity(intent);
+    }
+
+    private void prv2() {
+        Intent intent = new Intent(MainActivity.this, Company_Rule.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/barcodescanner/Termandcondition.java
@@ -0,0 +1,31 @@
+package com.example.barcodescanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Termandcondition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termandcondition);
+
+        Button button = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Agree();
+            }
+
+            private void Agree() {
+                Intent intent = new Intent(Termandcondition.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
