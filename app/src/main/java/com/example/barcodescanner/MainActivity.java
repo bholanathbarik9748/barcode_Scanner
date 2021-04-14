f3ff9897628f21cfcb0d0133dfe4734f2cb33e6f
Bholanath Barik <bholanathbarik9748@gmail.com>
2021-04-15 01:09:33 +0530

    implement libray for Scanner And crete ClickAct and Connct to server and improve docs

--- app/src/main/java/com/example/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.barcodescanner;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imageView, imageView1, imageView2;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView6);
         imageView1 = findViewById(R.id.imageView9);
         imageView2 = findViewById(R.id.imageView12);
+        imageView2 = findViewById(R.id.imageView12);
+        button = findViewById(R.id.button);
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +48,13 @@ public class MainActivity extends AppCompatActivity {
                 prv2();
             }
         });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prv3();
+            }
+        });
     }
 
     private void prv() {
@@ -53,5 +70,43 @@ public class MainActivity extends AppCompatActivity {
     private void prv2() {
         Intent intent = new Intent(MainActivity.this, Company_Rule.class);
         startActivity(intent);
+    }
+
+    private void prv3() {
+        IntentIntegrator intent = new IntentIntegrator(MainActivity.this);
+        intent.setCaptureActivity(clickAct.class);
+        intent.setOrientationLocked(false);
+        intent.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        intent.setPrompt("Scanning Code");
+        intent.initiateScan();
+    }
+
+
+    protected void OnActivityResult(int req_code, int res_code, Intent data) {
+        IntentResult res = IntentIntegrator.parseActivityResult(req_code, res_code, data);
+        if (res != null) {
+            if (res.getContents() != null) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(res.getContents());
+                builder.setTitle("Scanner result : ");
+                builder.setPositiveButton("Scan Again", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        prv3();
+                    }
+                }).setNegativeButton("Finish", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            } else {
+                Toast.makeText(this, "No Result", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(req_code, res_code, data);
+        }
     }
 }
